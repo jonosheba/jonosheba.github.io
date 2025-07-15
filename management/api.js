3a398c4f654310b00c4342bce5be964119489c65
jonosheba <jonosheba64@gmail.com>
2025-07-16 01:10:16 +0600

    Update api.js
    At app script sum to sumproduct on DPS

--- management/api.js
+++ management/api.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwnY9RL1838s4qYxapGQFFms05nQz4DIexi_codolKQZMaTf7NpPFgxLV7JlzWsZorP/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzvbXjzzz1iPeQ9w_nv8qbwP_yZ-qtJ5M8KB5fIfaGwmAe1yRDh4wcQ_yoZnQX59Toa/exec';
